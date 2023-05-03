29b55f3dfc3e31e27e9340ad19f82888c7219fce
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-03 18:53:30 +0530

    Add Site Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-800 text-white">
-      <h1 className="text-5xl font-black uppercase tracking-widest">
-        Dictionary Web App
-      </h1>
+    <div className="min-h-screen font-sansSerif">
+      <h1 className="text-3xl uppercase">Dictionary Web App</h1>
+
+      <ul className="mt-8 text-xl">
+        <li className="font-sansSerif">Sans Serif</li>
+        <li className="font-serif">Serif</li>
+        <li className="font-mono">Mono</li>
+      </ul>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grey: {
+          50: '#f4f4f4',
+          100: '#e9e9e9',
+          200: '#757575',
+          300: '#3a3a3a',
+          400: '#2d2d2d',
+          500: '#1f1f1f',
+          600: '#050505',
+        },
+        primary: {
+          purple: '#a445ed',
+          red: '#ff5252',
+        },
+      },
+      fontFamily: {
+        serif: "'Lora', serif",
+        sansSerif: "'Inter', sans-serif",
+        mono: "'Inconsolata', monospace",
+      },
+    },
   },
+  darkMode: 'class',
   plugins: [],
 };
