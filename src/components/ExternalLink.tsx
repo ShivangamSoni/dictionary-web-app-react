4b9c6629812d257f09240cb82446dbbe40235329
Shivangam Soni <shivangamsoni99@gmail.com>
2023-05-04 22:41:33 +0530

    Add External Link Component

---
+++ src/components/ExternalLink.tsx
@@ -0,0 +1,30 @@
+import { useState, ComponentProps } from 'react';
+
+import { FiExternalLink as ExternalLinkIcon } from 'react-icons/Fi';
+
+export default function ExternalLink({
+  children,
+  ...rest
+}: ComponentProps<'a'>) {
+  const [hover, setHover] = useState(false);
+
+  return (
+    <a
+      className="inline-flex items-center gap-2 peer"
+      {...rest}
+      target="_blank"
+      rel="noreferrer"
+      onMouseEnter={() => setHover(true)}
+      onMouseLeave={() => setHover(false)}
+    >
+      <span
+        className={`${
+          hover ? 'border-b border-grey-100 dark:border-grey-200' : ''
+        }`}
+      >
+        {children}
+      </span>
+      <ExternalLinkIcon />
+    </a>
+  );
+}
